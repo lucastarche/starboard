7e8cf41e48925c90be4e7c02bc3b1a6ad79c08c2
tarche <lucastarche@gmail.com>
2022-09-28 14:01:14 -0300

    Added app background

---
+++ src/app_background.rs
@@ -0,0 +1,50 @@
+use std::path::Path;
+
+use egui::{Color32, TextureHandle, Vec2};
+
+use crate::drawable::Drawable;
+
+#[derive(Default)]
+pub struct AppBackground {
+    texture: Option<TextureHandle>,
+}
+
+impl Drawable for AppBackground {
+    fn draw(&mut self, ui: &mut egui::Ui) {
+        let texture: &TextureHandle = self.texture.get_or_insert_with(|| {
+            if let Ok(color_image) = load_image_from_path(&Path::new("assets/background.jpg")) {
+                // TODO: Load file from a config or something
+                ui.ctx()
+                    .load_texture("background-image", color_image, egui::TextureFilter::Linear)
+            } else {
+                ui.ctx().load_texture(
+                    "background-image",
+                    egui::ColorImage::new([1920, 1080], Color32::from_rgb(0x28, 0x28, 0x28)),
+                    egui::TextureFilter::Linear,
+                )
+            }
+        });
+
+        ui.image(texture, texture.size_vec2());
+    }
+}
+
+impl AppBackground {
+    pub fn size(&self) -> Vec2 {
+        match &self.texture {
+            Some(t) => t.size_vec2(),
+            None => Vec2::ZERO,
+        }
+    }
+}
+
+fn load_image_from_path(path: &std::path::Path) -> Result<egui::ColorImage, image::ImageError> {
+    let image = image::io::Reader::open(path)?.decode()?;
+    let size = [image.width() as _, image.height() as _];
+    let image_buffer = image.to_rgba8();
+    let pixels = image_buffer.as_flat_samples();
+    Ok(egui::ColorImage::from_rgba_unmultiplied(
+        size,
+        pixels.as_slice(),
+    ))
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,20 @@
-pub struct MyApp {
-    name: String,
-    age: u32,
-}
+use app_background::AppBackground;
+use drawable::Drawable;
 
-impl Default for MyApp {
-    fn default() -> Self {
-        Self {
-            name: "Arthur".to_owned(),
-            age: 42,
-        }
-    }
+mod app_background;
+mod drawable;
+
+#[derive(Default)]
+pub struct MyApp {
+    background: AppBackground,
 }
 
 impl MyApp {
     pub fn update(&mut self, ctx: &egui::Context) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                self.age += 1;
-            }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
-        });
+        egui::Area::new("background")
+            .interactable(false)
+            .fixed_pos(ctx.available_rect().center() - self.background.size() / 2.0)
+            .order(egui::Order::Background)
+            .show(ctx, |ui| self.background.draw(ui));
     }
 }
