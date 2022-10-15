4696da379e4eaf6cced8837aa5d5dc2c808137a3
tarche <lucastarche@gmail.com>
2022-10-15 20:08:16 -0300

    The "Waifus deserve being shared with the world" update

--- gadgets/safebooru-waifu/src/lib.rs
+++ gadgets/safebooru-waifu/src/lib.rs
@@ -1,13 +1,13 @@
 use std::sync::{Arc, Mutex};
 
-use egui_extras::RetainedImage;
-use safebooru::query_random_image;
+use egui::{CursorIcon, Image, Sense};
+use safebooru::{query_random_image, ImageWithMetadata};
 use utils::{Gadget, GadgetFactory, MutexExt};
 
 mod safebooru;
 
 pub struct WaifuGadget {
-    image: Arc<Mutex<Option<RetainedImage>>>,
+    image: Arc<Mutex<Option<ImageWithMetadata>>>,
 }
 
 pub struct WaifuGadgetFactory;
@@ -15,14 +15,8 @@ pub struct WaifuGadgetFactory;
 impl Gadget for WaifuGadget {
     fn render(&mut self, ctx: &egui::Context) {
         egui::Window::new("Your daily waifu").show(ctx, |ui| {
-            if let Some(image) = &*self.image.locked() {
-                let available_size = ui.available_size();
-                let x_scale = image.size_vec2().x / available_size.x;
-                let y_scale = image.size_vec2().y / available_size.y;
-
-                ui.centered_and_justified(|ui| {
-                    image.show_size(ui, image.size_vec2() / x_scale.max(y_scale));
-                });
+            if let Some(image_with_metadata) = &*self.image.locked() {
+                render_waifu(ui, image_with_metadata);
             }
         });
     }
@@ -57,4 +51,35 @@ impl GadgetFactory for WaifuGadgetFactory {
 
         Box::new(waifu_gadget)
     }
+}
+
+fn render_waifu(ui: &mut egui::Ui, image_with_metadata: &ImageWithMetadata) {
+    let available_size = ui.available_size();
+
+    let image = &image_with_metadata.inner;
+    let x_scale = image.size_vec2().x / available_size.x;
+    let y_scale = image.size_vec2().y / available_size.y;
+
+    ui.centered_and_justified(|ui| {
+        let button = Image::new(
+            image.texture_id(ui.ctx()),
+            image.size_vec2() / x_scale.max(y_scale),
+        )
+        .sense(Sense::click());
+
+        let response = ui.add(button).on_hover_cursor(CursorIcon::PointingHand);
+
+        if response.clicked() {
+            ui.ctx()
+                .output()
+                .open_url(image_with_metadata.post_url.clone());
+        }
+
+        response.context_menu(|ui| {
+            if ui.button("Copy image URL").clicked() {
+                ui.ctx().output().copied_text = image_with_metadata.file_url.clone();
+                ui.close_menu();
+            }
+        });
+    });
 }
--- gadgets/safebooru-waifu/src/safebooru.rs
+++ gadgets/safebooru-waifu/src/safebooru.rs
@@ -3,16 +3,24 @@ use serde::{Deserialize, Serialize};
 use utils::RetainedImageError;
 
 const QUERY_URL: &str = "https://safebooru.donmai.us/posts/random.json?tags=1girl";
+const POST_URL: &str = "https://safebooru.donmai.us/posts/";
 
 #[derive(Serialize, Deserialize)]
 struct SafebooruResponse {
-    pub file_url: String,
+    id: u64,
+    file_url: String,
 }
 
-pub async fn query_random_image() -> anyhow::Result<RetainedImage> {
+pub struct ImageWithMetadata {
+    pub inner: RetainedImage,
+    pub file_url: String,
+    pub post_url: String,
+}
+
+pub async fn query_random_image() -> anyhow::Result<ImageWithMetadata> {
     let response = reqwest::get(QUERY_URL).await?.text().await?;
     let response: SafebooruResponse = serde_json::from_str(&response)?;
-    let image_response = reqwest::get(response.file_url).await?;
+    let image_response = reqwest::get(&response.file_url).await?;
 
     let image = RetainedImage::from_image_bytes(
         image_response.url().to_string(),
@@ -20,5 +28,9 @@ pub async fn query_random_image() -> anyhow::Result<RetainedImage> {
     )
     .map_err(RetainedImageError)?;
 
-    Ok(image)
+    Ok(ImageWithMetadata {
+        inner: image,
+        file_url: response.file_url,
+        post_url: format!("{}{}", POST_URL, response.id),
+    })
 }
