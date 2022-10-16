80b1e656c1e4f06d02b9110613d7ac613e4fa23f
Mițca Dumitru <dumitru0mitca@gmail.com>
2022-10-16 17:44:06 +0300

    cses-status: Retrieve the UID from the config file

--- gadgets/cses-status/src/lib.rs
+++ gadgets/cses-status/src/lib.rs
@@ -1,16 +1,55 @@
 use std::sync::{Arc, Mutex};
 
 use cses_query::{Problem, ProblemStatus};
-use egui::{Color32, CursorIcon, RichText, Sense};
+use egui::{Color32, CursorIcon, RichText, Sense, TextEdit};
+use serde::{Deserialize, Serialize};
 use utils::{Gadget, GadgetFactory, MutexExt};
 
 mod cses_query;
 
 pub struct CSESStatusGadget {
-    problem_data: Arc<Mutex<Vec<Problem>>>,
+    problem_data: ProblemData,
+    network_runtime: utils::NetworkRuntime,
+    should_ask_for_id: bool,
+    input: String,
 }
 
+type ProblemData = Arc<Mutex<Vec<Problem>>>;
+
 pub struct CSESStatusGadgetFactory;
+
+#[derive(Serialize, Deserialize)]
+struct CSESStatusConfig {
+    #[serde(rename = "user-id")]
+    user_id: u64,
+}
+
+impl CSESStatusGadget {
+    fn ask_for_id(&mut self, ui: &mut egui::Ui) {
+        ui.horizontal(|ui| {
+            ui.label(RichText::new("Configure your CSES UID: "));
+
+            let text_color = self.input.parse::<u64>().err().map(|_| Color32::RED);
+
+            let text_input = TextEdit::singleline(&mut self.input).text_color_opt(text_color);
+            let response = ui.add(text_input);
+
+            if response.lost_focus() && ui.input().key_pressed(egui::Key::Enter) {
+                if let Ok(user_id) = self.input.parse::<u64>() {
+                    utils::update_config_for_gadget(self, CSESStatusConfig { user_id }).unwrap();
+                    self.network_runtime.spawn(get_problem_data(
+                        self.problem_data.clone(),
+                        ui.ctx().clone(),
+                        user_id,
+                    ));
+                    self.should_ask_for_id = false;
+                } else {
+                    // Do nothing this frame, next one the text will become red
+                }
+            }
+        });
+    }
+}
 
 impl Gadget for CSESStatusGadget {
     fn id(&self) -> &'static str {
@@ -22,7 +61,13 @@ impl Gadget for CSESStatusGadget {
             .min_width(200.0)
             .show(ctx, |ui| {
                 egui::ScrollArea::vertical().show(ui, |ui| {
-                    ui.horizontal_wrapped(|ui| render_problems(ui, &self.problem_data.locked()));
+                    if self.should_ask_for_id {
+                        self.ask_for_id(ui);
+                    } else {
+                        ui.horizontal_wrapped(|ui| {
+                            render_problems(ui, &self.problem_data.locked())
+                        });
+                    }
                 });
             });
     }
@@ -38,26 +83,38 @@ impl GadgetFactory for CSESStatusGadgetFactory {
         network_runtime: &utils::NetworkRuntime,
         egui_ctx: &egui::Context,
     ) -> Box<dyn Gadget> {
-        let cses_status_gadget = CSESStatusGadget {
+        let mut cses_status_gadget = CSESStatusGadget {
             problem_data: Arc::new(Mutex::new(vec![])),
+            network_runtime: network_runtime.clone(),
+            should_ask_for_id: false,
+            input: String::new(),
         };
 
-        let problem_lock = cses_status_gadget.problem_data.clone();
-        let ctx = egui_ctx.clone();
-        network_runtime.spawn(async move {
-            // TODO: Third time we ask for this, allow configuring user ID
-            let problem_data = cses_query::query_user_status(5418).await;
+        let config = utils::config_for_gadget::<CSESStatusConfig>(&cses_status_gadget);
 
-            match problem_data {
-                Ok(problem_data) => *problem_lock.locked() = problem_data,
-                Err(e) => println!("Failed to get user status from cses.fi: {e}"),
-            }
-
-            ctx.request_repaint();
-        });
+        if let Ok(config) = config {
+            network_runtime.spawn(get_problem_data(
+                cses_status_gadget.problem_data.clone(),
+                egui_ctx.clone(),
+                config.user_id,
+            ));
+        } else {
+            cses_status_gadget.should_ask_for_id = true;
+        }
 
         Box::new(cses_status_gadget)
     }
+}
+
+async fn get_problem_data(problem_lock: ProblemData, ctx: egui::Context, user_id: u64) {
+    let problem_data = cses_query::query_user_status(user_id).await;
+
+    match problem_data {
+        Ok(problem_data) => *problem_lock.locked() = problem_data,
+        Err(e) => println!("Failed to get user status from cses.fi: {e}"),
+    }
+
+    ctx.request_repaint();
 }
 
 fn render_problems(ui: &mut egui::Ui, problems: &[Problem]) {
