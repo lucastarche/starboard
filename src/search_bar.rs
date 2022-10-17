042d69636e88647c086e460f841428610a411685
tarche <lucastarche@gmail.com>
2022-10-17 20:41:27 -0300

    Close SearchBar if it loses focus, request it when drawn

--- src/search_bar.rs
+++ src/search_bar.rs
@@ -1,4 +1,4 @@
-use egui::Color32;
+use egui::{Color32, Key};
 
 use crate::gadgets;
 
@@ -28,7 +28,9 @@ impl SearchBar {
     }
 
     fn draw_search_bar(&mut self, ui: &mut egui::Ui) {
-        ui.text_edit_singleline(&mut self.search_query);
+        let response = ui.text_edit_singleline(&mut self.search_query);
+        self.handle_input(ui, &response);
+
         let possible_gadgets = gadgets::GADGET_FACTORIES.iter().filter_map(|gadget| {
             if gadget.gadget_name().starts_with(&self.search_query) {
                 Some(gadget.gadget_name())
@@ -46,5 +48,21 @@ impl SearchBar {
         if !any_matched {
             ui.colored_label(Color32::RED, "Could not find any gadget");
         }
+    }
+
+    fn handle_input(&mut self, ui: &egui::Ui, edit_response: &egui::Response) {
+        let pressed_tab = ui.input().key_pressed(Key::Tab);
+        let pressed_enter = ui.input().key_pressed(Key::Enter);
+        let should_close = edit_response.lost_focus() && !pressed_tab && !pressed_enter;
+        // TODO: Show current selection
+        // TODO: Handle tab and shift-tab for changing selection
+        // TODO: Handle enter for opening selected gadget
+
+        if should_close {
+            self.is_open = false;
+            return;
+        }
+
+        edit_response.request_focus();
     }
 }
