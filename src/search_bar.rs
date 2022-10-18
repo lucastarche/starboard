22bbdecdba172a9ab8f2fdf5dd618265f7f609fa
tarche <lucastarche@gmail.com>
2022-10-18 13:54:12 -0300

    Show current selection for searchbar

--- src/search_bar.rs
+++ src/search_bar.rs
@@ -1,4 +1,4 @@
-use egui::{Color32, Key, TextStyle};
+use egui::{Color32, Key, ScrollArea, TextStyle};
 use utils::GadgetFactory;
 
 use crate::gadgets;
@@ -8,6 +8,7 @@ pub struct SearchBar {
     is_open: bool,
     is_first_frame: bool,
     search_query: String,
+    selected: usize,
 
     matching_gadgets: Vec<&'static (dyn GadgetFactory + Sync)>,
 }
@@ -37,9 +38,21 @@ impl SearchBar {
         let response = ui.text_edit_singleline(&mut self.search_query);
         self.handle_input(ui, &response);
 
-        for gadget in self.matching_gadgets.iter() {
-            ui.label(gadget.gadget_name());
-        }
+        ScrollArea::vertical().show_rows(
+            ui,
+            ui.text_style_height(&TextStyle::Heading),
+            self.matching_gadgets.len(),
+            |ui, row_range| {
+                for row in row_range {
+                    if row == self.selected {
+                        ui.colored_label(Color32::YELLOW, self.matching_gadgets[row].gadget_name())
+                            .scroll_to_me(None);
+                    } else {
+                        ui.label(self.matching_gadgets[row].gadget_name());
+                    }
+                }
+            },
+        );
 
         if self.matching_gadgets.is_empty() {
             ui.colored_label(Color32::RED, "Could not find any gadget");
@@ -49,9 +62,8 @@ impl SearchBar {
     fn handle_input(&mut self, ui: &egui::Ui, edit_response: &egui::Response) {
         let pressed_tab = ui.input().key_pressed(Key::Tab);
         let pressed_enter = ui.input().key_pressed(Key::Enter);
+        let pressed_shift = ui.input().modifiers.shift;
         let should_close = edit_response.lost_focus() && !pressed_tab && !pressed_enter;
-        // TODO: Show current selection
-        // TODO: Handle tab and shift-tab for changing selection
         // TODO: Handle enter for opening selected gadget
 
         if should_close {
@@ -67,8 +79,32 @@ impl SearchBar {
                 .collect();
 
             self.is_first_frame = false;
+            self.selected = self.selected.clamp(0, self.matching_gadgets.len() - 1);
+        }
+
+        if (pressed_tab && !pressed_shift) || ui.input().key_pressed(Key::ArrowDown) {
+            self.select_next();
+        }
+        if (pressed_tab && pressed_shift) || ui.input().key_pressed(Key::ArrowUp) {
+            self.select_prev();
         }
 
         edit_response.request_focus();
+    }
+
+    fn select_next(&mut self) {
+        if self.selected + 1 >= self.matching_gadgets.len() {
+            self.selected = 0;
+        } else {
+            self.selected += 1;
+        }
+    }
+
+    fn select_prev(&mut self) {
+        if self.selected == 0 {
+            self.selected = self.matching_gadgets.len().saturating_sub(1);
+        } else {
+            self.selected -= 1;
+        }
     }
 }
